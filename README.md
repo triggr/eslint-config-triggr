8b65e369c3e5805df936186aaaea554bbfdabd1a
Jacob Lee <artdent@gmail.com>
2017-07-10 10:28:17 -0500

    Initial commit.
    ESLintrc forked from triggr/triggr_api.

---
+++ README.md
@@ -0,0 +1,10 @@
+## eslint-config-triggr
+
+This package includes common ESLint rules that should be enabled for
+all Triggr Health projects. Projects must also inherit from a base
+ruleset, such as eslint:recommended.
+
+Some legacy code still uses jshint+jscs, but newer projects use ESLint.
+
+See http://eslint.org/docs/developer-guide/shareable-configs for a
+description of ESLint shareable configs.
---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  rules: {
+    'curly': 'error',
+    'eqeqeq': 'error',
+    'max-len': [
+      'error', {
+        code: 100,
+        ignoreUrls: true,
+        ignorePattern: 'const .+ = require\\('
+      }
+    ],
+    'new-cap': ['error', {properties: false}],
+    'no-empty': 'off',
+    'no-implicit-coercion': 'error',
+    'no-multi-spaces': 'error',
+    'no-tabs': 'error',
+    'no-throw-literal': 'error',
+    'no-trailing-spaces': 'error',
+    'no-unused-vars': ['error', {args: 'none'}],
+    'no-var': 'error',
+    'object-curly-spacing': 'error',
+    'prefer-promise-reject-errors': 'error',
+    'quotes': ["error", "single", {avoidEscape: true, allowTemplateLiterals: true}],
+    'semi': 'error',
+    'space-before-function-paren': ['error', 'never'],
+    'wrap-iife': 'error',
+  },
+};
