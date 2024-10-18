cad8ab530e152ec8dd529f1f3a0092589158a146
Anthony Vespoli <avespoli@sonarmd.com>
2024-10-18 15:09:54 -0500

    changing eslint config to not care about max len in comments

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 module.exports = {
   rules: {
-    'arrow-parens': 'error',
-    'curly': 'error',
-    'eqeqeq': 'error',
-    'max-len': [
-      'error', {
+    "arrow-parens": "error",
+    curly: "error",
+    eqeqeq: "error",
+    "max-len": [
+      "error",
+      {
         code: 100,
         ignoreUrls: true,
-        ignorePattern: 'const .+ = require\\('
-      }
+        ignorePattern: "const .+ = require\\(",
+        ignoreComments: true,
+      },
     ],
-    'new-cap': ['error', {properties: false}],
-    'no-empty': 'off',
-    'no-implicit-coercion': 'error',
-    'no-multi-spaces': 'error',
-    'no-tabs': 'error',
-    'no-throw-literal': 'error',
-    'no-trailing-spaces': 'error',
-    'no-unused-vars': ['error', {args: 'none', ignoreRestSiblings: true}],
-    'no-var': 'error',
-    'object-curly-spacing': 'error',
-    'prefer-promise-reject-errors': 'error',
-    'quotes': ["error", "single", {avoidEscape: true, allowTemplateLiterals: true}],
-    'semi': 'error',
-    'space-before-function-paren': ['error', {
-      anonymous: 'never',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
-    'strict': 'error',
-    'wrap-iife': 'error',
+    "new-cap": ["error", { properties: false }],
+    "no-empty": "off",
+    "no-implicit-coercion": "error",
+    "no-multi-spaces": "error",
+    "no-tabs": "error",
+    "no-throw-literal": "error",
+    "no-trailing-spaces": "error",
+    "no-unused-vars": ["error", { args: "none", ignoreRestSiblings: true }],
+    "no-var": "error",
+    "object-curly-spacing": "error",
+    "prefer-promise-reject-errors": "error",
+    quotes: [
+      "error",
+      "single",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    semi: "error",
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+    strict: "error",
+    "wrap-iife": "error",
   },
 };
